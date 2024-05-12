15df7016e7b9dee1eb98981b5ddd4663cc1b7e0c
kuanysh-beisembayev <kuanysh.beisembayev@gmail.com>
2024-05-13 02:12:04 +0500

    fix: fix navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TaskModule from "./modules/TaskModule";
 import AuthModule from "./modules/AuthModule";
 import AuthRequired from "./modules/AuthModule/components/AuthRequired";
 import ProfileModule from "./modules/ProfileModule";
+import Layout from "./modules/Shared/components/Layout";
 
 const App = () => {
   return (
@@ -14,16 +15,18 @@ const App = () => {
           nest
         />
         <AuthRequired>
-          <Route
-            path="/profile"
-            component={ProfileModule}
-            nest
-          />
-          <Route
-            path="/"
-            component={TaskModule}
-            nest
-          />
+          <Layout withNavbar>
+            <Route
+              path="/profile"
+              component={ProfileModule}
+              nest
+            />
+            <Route
+              path="/"
+              component={TaskModule}
+              nest
+            />
+          </Layout>
         </AuthRequired>
       </Switch>
     </Router>
--- src/modules/TaskModule/index.tsx
+++ src/modules/TaskModule/index.tsx
@@ -2,26 +2,23 @@ import { Route, Switch } from "wouter";
 import TaskListPage from "./pages/TaskListPage";
 import TaskCreatePage from "./pages/TaskCreatePage";
 import TaskDetailPage from "./pages/TaskDetailPage";
-import Layout from "../Shared/components/Layout";
 
 const TaskModule = () => {
   return (
-    <Layout withNavbar>
-      <Switch>
-        <Route
-          path="/"
-          component={TaskListPage}
-        />
-        <Route
-          path="/task/new"
-          component={TaskCreatePage}
-        />
-        <Route
-          path="/task/:taskId"
-          component={TaskDetailPage}
-        />
-      </Switch>
-    </Layout>
+    <Switch>
+      <Route
+        path="/"
+        component={TaskListPage}
+      />
+      <Route
+        path="/task/new"
+        component={TaskCreatePage}
+      />
+      <Route
+        path="/task/:taskId"
+        component={TaskDetailPage}
+      />
+    </Switch>
   );
 };
 
